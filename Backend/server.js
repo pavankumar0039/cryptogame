6341116e9a3c040220d2420805b4e550cb55dfe2
Pavankumarrise11 <129648741+pavankumar0039@users.noreply.github.com>
2025-07-28 16:35:25 +0530

    hello commit cors

--- Backend/server.js
+++ Backend/server.js
@@ -11,14 +11,27 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
+const allowedOrigins = ['https://cryptogame-phi.vercel.app'];
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: allowedOrigins,
     methods: ['GET', 'POST'],
   },
 });
 
-app.use(cors());
+
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST'],
+}));
 app.use(express.json());
 app.use('/api/game', gameRoutes);
 
@@ -27,9 +40,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch(err => console.log(err));
 
 
-const rooms = {}; 
-const socketToRoom = {}; 
-const userToRoom = {};  
+const rooms = {};
+const socketToRoom = {};
+const userToRoom = {};
 const roomLimit = 5;
 
 io.on('connection', (socket) => {
